78bf473537d3d9fff0d76b2724da041e99ace2f6
K9i-0 <geukfyger@gmail.com>
2023-04-05 18:04:19 +0900

    コードフォーマットのタスクを追加

--- lib/src/toridori_grinder_tasks.dart
+++ lib/src/toridori_grinder_tasks.dart
@@ -1,10 +1,28 @@
 import 'package:grinder/grinder.dart';
 
-void addSampleTask() {
+void addAllTasks() {
+  addFormatTask();
+}
+
+void addFormatTask() {
   addTask(
     GrinderTask(
-      'sample',
-      taskFunction: () {},
+      'format',
+      description: 'コードのフォーマット',
+      taskFunction: () => _format(),
     ),
   );
+}
+
+String _format() {
+  final result = run(
+    '/bin/sh',
+    arguments: [
+      '-c',
+      r'find . -type f \( ! -name "*.freezed.dart" -and ! -name "*.g.dart" -and ! -name "*.graphql.dart" \) -and -name "*.dart" | xargs dart format --fix -l 200',
+    ],
+    workingDirectory: 'lib',
+  );
+
+  return result;
 }
