b837869e0b18c134958cc0c35f93db697ac41374
K9i-0 <geukfyger@gmail.com>
2023-04-05 17:47:56 +0900

    init package

---
+++ example/toridori_grinder_tasks_example.dart
@@ -0,0 +1,6 @@
+import 'package:toridori_grinder_tasks/toridori_grinder_tasks.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/toridori_grinder_tasks.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library toridori_grinder_tasks;
+
+export 'src/toridori_grinder_tasks_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
